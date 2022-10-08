2eab20d90c90865ac8f13251fa1de4c884df09f1
shawticus <shawmakesmagic@gmail.com>
2022-10-08 01:14:55 -0700

    init commit of blender cycles/eevee split with renderer scenes for debugging

--- render_shapenet_data/render_all.py
+++ render_shapenet_data/render_all.py
@@ -8,37 +8,101 @@
 
 import os
 import argparse
+import json
 
 parser = argparse.ArgumentParser(description='Renders given obj file by rotation a camera around it.')
 parser.add_argument(
-    '--save_folder', type=str, default='./tmp',
+    '--save_folder', type=str, default='./shapenet_rendered',
     help='path for saving rendered image')
 parser.add_argument(
-    '--dataset_folder', type=str, default='./tmp',
-    help='path for downloaded 3d dataset folder')
+    '--dataset_list', type=str, default='./dataset_list.json',
+    help='path to a json linking datasets')
 parser.add_argument(
-    '--blender_root', type=str, default='./tmp',
+    '--blender_root', type=str, default='blender',
     help='path for blender')
+parser.add_argument(
+    '--shapenet_version', type=str, default='2',
+    help='ShapeNet version 1 or 2')
+parser.add_argument(
+    '--num_views', type=str, default='24',
+    help='Number of views to capture per object')
+parser.add_argument(
+    '--engine', type=str, default='CYCLES',
+    help='Use CYCLES or EEVEE - CYCLES is a realistic path tracer (slow), EEVEE is a real-time renderer (fast)')
+parser.add_argument(
+    '--quiet_mode', type=bool, default=1,
+    help='Route output of console to log file')
 args = parser.parse_args()
 
+engine = args.engine
 save_folder = args.save_folder
-dataset_folder = args.dataset_folder
+dataset_list = args.dataset_list
 blender_root = args.blender_root
+shapenet_version = args.shapenet_version
+num_views = args.num_views
 
-synset_list = [
-    '02958343',  # Car
-    '03001627',  # Chair
-    '03790512'  # Motorbike
-]
-scale_list = [
-    0.9,
-    0.7,
-    0.9
-]
-for synset, obj_scale in zip(synset_list, scale_list):
-    file_list = sorted(os.listdir(os.path.join(dataset_folder, synset)))
+# check if dataset_list exists, throw error if not
+if not os.path.exists(dataset_list):
+    raise ValueError('dataset_list does not exist!')
+
+# check if save_folder exists
+if not os.path.exists(save_folder):
+    os.makedirs(save_folder)
+
+synset_list = []
+scale_list = []
+path_list = []
+
+# read and parse json file at dataset_list.json
+with open(dataset_list, 'r') as f:
+    dataset = json.load(f)
+
+# example json entry:
+#   {
+#     "name": "Car",
+#     "id": "02958343",
+#     "scale": 0.9,
+#     "directory": "./shapenet/02958343"
+#   }
+for entry in dataset:
+    synset_list.append(entry['id'])
+    scale_list.append(entry['scale'])
+    path_list.append(entry['directory'])
+
+
+# for shapenet v2, we normalize the model location
+if shapenet_version == '2':
+    for synset, obj_scale, dataset_folder in zip(synset_list, scale_list, path_list):
+        file_list = sorted(os.listdir(os.path.join(dataset_folder, synset)))
+        for file in file_list:
+            # check if file_list+'/models' exists
+            if os.path.exists(os.path.join(dataset_folder, synset, file, 'models')):
+                # move all files in file_list+'/models' to file_list
+                os.system('mv ' + os.path.join(dataset_folder, synset, file, 'models/*') + ' ' + os.path.join(dataset_folder, synset, file))
+                # remove file_list+'/models' if it exists
+                os.system('rm -rf ' + os.path.join(dataset_folder, synset, file, 'models'))
+            material_file = os.path.join(dataset_folder, synset, file, 'model_normalized.mtl')
+            # read material_file as a text file, replace any instance of '../images' with './images'
+            with open(material_file, 'r') as f:
+                material_file_text = f.read()
+            material_file_text = material_file_text.replace('../images', './images')
+            # write the modified text to material_file
+            with open(material_file, 'w') as f:
+                f.write(material_file_text)
+
+# ShapeNetCore v2 normalizes the scale and orientation of the models and the names are changed as a result
+model_name = 'model.obj'
+if shapenet_version == '2':
+    model_name = 'model_normalized.obj'
+
+suffix = ''
+if(args.quiet_mode):
+    suffix = ' >> tmp.out'
+
+for obj_scale, dataset_folder in zip(scale_list, path_list):
+    file_list = sorted(os.listdir(os.path.join(dataset_folder)))
     for idx, file in enumerate(file_list):
-        render_cmd = '%s -b -P render_shapenet.py -- --output %s %s  --scale %f --views 24 --resolution 1024 >> tmp.out' % (
-            blender_root, save_folder, os.path.join(dataset_folder, synset, file, 'model.obj'), obj_scale
+        render_cmd = '%s -b -P render_shapenet.py -- --output %s %s  --scale %f --views %s --resolution 1024 --engine %s' + suffix % (
+            blender_root, save_folder, os.path.join(dataset_folder, file, model_name), obj_scale, num_views, engine
         )
         os.system(render_cmd)
--- render_shapenet_data/render_shapenet.py
+++ render_shapenet_data/render_shapenet.py
@@ -8,9 +8,8 @@
 
 import argparse, sys, os, math, re
 import bpy
-from mathutils import Vector, Matrix
+from mathutils import Vector
 import numpy as np
-import json 
 
 parser = argparse.ArgumentParser(description='Renders given obj file by rotation a camera around it.')
 parser.add_argument(
@@ -29,74 +28,21 @@ parser.add_argument(
     '--format', type=str, default='PNG',
     help='Format of files generated. Either PNG or OPEN_EXR')
 parser.add_argument(
-    '--resolution', type=int, default=512,
-    help='Resolution of the images.')
-parser.add_argument(
     '--engine', type=str, default='CYCLES',
-    help='Blender internal engine for rendering. E.g. CYCLES, BLENDER_EEVEE, ...')
+    help='Blender internal engine for rendering. either CYCLES or EEVEE, ...')
 
 argv = sys.argv[sys.argv.index("--") + 1:]
 args = parser.parse_args(argv)
+
+if args.engine == 'CYCLES':
+    bpy.ops.wm.open_mainfile(filepath=os.path.abspath("./blender/cycles_renderer.blend"))
+else:
+    bpy.ops.wm.open_mainfile(filepath=os.path.abspath("./blender/eevee_renderer.blend"))
 
 # Set up rendering
 context = bpy.context
 scene = bpy.context.scene
 render = bpy.context.scene.render
-
-render.engine = args.engine
-render.image_settings.color_mode = 'RGBA'  # ('RGB', 'RGBA', ...)
-render.image_settings.file_format = args.format  # ('PNG', 'OPEN_EXR', 'JPEG, ...)
-render.resolution_x = args.resolution
-render.resolution_y = args.resolution
-render.resolution_percentage = 100
-bpy.context.scene.cycles.filter_width = 0.01
-bpy.context.scene.render.film_transparent = True
-
-bpy.context.scene.cycles.device = 'GPU'
-bpy.context.scene.cycles.diffuse_bounces = 1
-bpy.context.scene.cycles.glossy_bounces = 1
-bpy.context.scene.cycles.transparent_max_bounces = 3
-bpy.context.scene.cycles.transmission_bounces = 3
-bpy.context.scene.cycles.samples = 32
-bpy.context.scene.cycles.use_denoising = True
-
-
-def enable_cuda_devices():
-    prefs = bpy.context.preferences
-    cprefs = prefs.addons['cycles'].preferences
-    cprefs.get_devices()
-
-    # Attempt to set GPU device types if available
-    for compute_device_type in ('CUDA', 'OPENCL', 'NONE'):
-        try:
-            cprefs.compute_device_type = compute_device_type
-            print("Compute device selected: {0}".format(compute_device_type))
-            break
-        except TypeError:
-            pass
-
-    # Any CUDA/OPENCL devices?
-    acceleratedTypes = ['CUDA', 'OPENCL']
-    accelerated = any(device.type in acceleratedTypes for device in cprefs.devices)
-    print('Accelerated render = {0}'.format(accelerated))
-
-    # If we have CUDA/OPENCL devices, enable only them, otherwise enable
-    # all devices (assumed to be CPU)
-    print(cprefs.devices)
-    for device in cprefs.devices:
-        device.use = not accelerated or device.type in acceleratedTypes
-        print('Device enabled ({type}) = {enabled}'.format(type=device.type, enabled=device.use))
-
-    return accelerated
-
-
-enable_cuda_devices()
-context.active_object.select_set(True)
-bpy.ops.object.delete()
-
-# Import textured mesh
-bpy.ops.object.select_all(action='DESELECT')
-
 
 def bounds(obj, local=False):
     local_coords = obj.bound_box[:]
@@ -125,39 +71,6 @@ def bounds(obj, local=False):
     o_details = collections.namedtuple('object_details', 'x y z')
     return o_details(**originals)
 
-# function from https://github.com/panmari/stanford-shapenet-renderer/blob/master/render_blender.py
-def get_3x4_RT_matrix_from_blender(cam):
-    # bcam stands for blender camera
-    # R_bcam2cv = Matrix(
-    #     ((1, 0,  0),
-    #     (0, 1, 0),
-    #     (0, 0, 1)))
-
-    # Transpose since the rotation is object rotation, 
-    # and we want coordinate rotation
-    # R_world2bcam = cam.rotation_euler.to_matrix().transposed()
-    # T_world2bcam = -1*R_world2bcam @ location
-    #
-    # Use matrix_world instead to account for all constraints
-    location, rotation = cam.matrix_world.decompose()[0:2]
-    R_world2bcam = rotation.to_matrix().transposed()
-
-    # Convert camera location to translation vector used in coordinate changes
-    # T_world2bcam = -1*R_world2bcam @ cam.location
-    # Use location from matrix_world to account for constraints:     
-    T_world2bcam = -1*R_world2bcam @ location
-
-    # # Build the coordinate transform matrix from world to computer vision camera
-    # R_world2cv = R_bcam2cv@R_world2bcam
-    # T_world2cv = R_bcam2cv@T_world2bcam
-
-    # put into 3x4 matrix
-    RT = Matrix((
-        R_world2bcam[0][:] + (T_world2bcam[0],),
-        R_world2bcam[1][:] + (T_world2bcam[1],),
-        R_world2bcam[2][:] + (T_world2bcam[2],)
-        ))
-    return RT
 
 imported_object = bpy.ops.import_scene.obj(filepath=args.obj, use_edges=False, use_smooth_groups=False, split_mode='OFF')
 
@@ -191,91 +104,36 @@ mesh_obj.scale[1] /= factor
 mesh_obj.scale[2] /= factor
 bpy.ops.object.transform_apply(scale=True)
 
-bpy.ops.object.light_add(type='AREA')
-light2 = bpy.data.lights['Area']
-
-light2.energy = 30000
-bpy.data.objects['Area'].location[2] = 0.5
-bpy.data.objects['Area'].scale[0] = 100
-bpy.data.objects['Area'].scale[1] = 100
-bpy.data.objects['Area'].scale[2] = 100
-
-# Place camera
+# Get reference to camera and empty (rotation pivot)
 cam = scene.objects['Camera']
-cam.location = (0, 1.2, 0)  # radius equals to 1
-cam.data.lens = 35
-cam.data.sensor_width = 32
-
-cam_constraint = cam.constraints.new(type='TRACK_TO')
-cam_constraint.track_axis = 'TRACK_NEGATIVE_Z'
-cam_constraint.up_axis = 'UP_Y'
-
-cam_empty = bpy.data.objects.new("Empty", None)
-cam_empty.location = (0, 0, 0)
-cam.parent = cam_empty
-
-scene.collection.objects.link(cam_empty)
-context.view_layer.objects.active = cam_empty
-cam_constraint.target = cam_empty
+cam_empty = scene.objects['Empty']
 
 stepsize = 360.0 / args.views
 rotation_mode = 'XYZ'
 
 model_identifier = os.path.split(os.path.split(args.obj)[0])[1]
+print('model identifier: ' + model_identifier)
 synset_idx = args.obj.split('/')[-3]
+print('synset idx: ' + synset_idx)
 
-img_follder = os.path.join(os.path.abspath(args.output_folder), 'img', synset_idx, model_identifier)
-camera_follder = os.path.join(os.path.abspath(args.output_folder), 'camera', synset_idx, model_identifier)
+img_folder = os.path.join(os.path.abspath(args.output_folder), 'img', synset_idx, model_identifier)
+camera_folder = os.path.join(os.path.abspath(args.output_folder), 'camera', synset_idx, model_identifier)
 
-os.makedirs(img_follder, exist_ok=True)
-os.makedirs(camera_follder, exist_ok=True)
+os.makedirs(img_folder, exist_ok=True)
+os.makedirs(camera_folder, exist_ok=True)
 
 rotation_angle_list = np.random.rand(args.views)
 elevation_angle_list = np.random.rand(args.views)
 rotation_angle_list = rotation_angle_list * 360
 elevation_angle_list = elevation_angle_list * 30
-np.save(os.path.join(camera_follder, 'rotation'), rotation_angle_list)
-np.save(os.path.join(camera_follder, 'elevation'), elevation_angle_list)
-
-# creation of the transform.json
-to_export = {
-    'camera_angle_x': bpy.data.cameras[0].angle_x,
-    "aabb": [[-scale/2,-scale/2,-scale/2],
-             [scale/2,scale/2,scale/2]]
-}
-frames = [] 
+np.save(os.path.join(camera_folder, 'rotation'), rotation_angle_list)
+np.save(os.path.join(camera_folder, 'elevation'), elevation_angle_list)
 
 for i in range(0, args.views):
     cam_empty.rotation_euler[2] = math.radians(rotation_angle_list[i])
     cam_empty.rotation_euler[0] = math.radians(elevation_angle_list[i])
 
     print("Rotation {}, {}".format((stepsize * i), math.radians(stepsize * i)))
-    render_file_path = os.path.join(img_follder, '%03d.png' % (i))
+    render_file_path = os.path.join(img_folder, '%03d.png' % (i))
     scene.render.filepath = render_file_path
     bpy.ops.render.render(write_still=True)
-    # might not need it, but just in case cam is not updated correctly
-    bpy.context.view_layer.update()
-
-    rt = get_3x4_RT_matrix_from_blender(cam)
-    pos, rt, scale = cam.matrix_world.decompose()
-
-    rt = rt.to_matrix()
-    matrix = []
-    for ii in range(3):
-        a = []
-        for jj in range(3):
-            a.append(rt[ii][jj])
-        a.append(pos[ii])
-        matrix.append(a)
-    matrix.append([0,0,0,1])
-    print(matrix)
-
-    to_add = {\
-        "file_path":f'{str(i).zfill(3)}.png',
-        "transform_matrix":matrix
-    }
-    frames.append(to_add)
-
-to_export['frames'] = frames
-with open(f'{img_follder}/transforms.json', 'w') as f:
-    json.dump(to_export, f,indent=4)    
