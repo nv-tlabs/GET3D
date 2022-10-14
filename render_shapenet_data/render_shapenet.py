9400a6dd06b492a9d439e710a202b8f18f49480e
Aman Kishore <aman@mirageml.com>
2022-10-14 02:22:39 +0000

    Parallel Processing

--- render_shapenet_data/render_shapenet.py
+++ render_shapenet_data/render_shapenet.py
@@ -30,6 +30,9 @@ parser.add_argument(
 parser.add_argument(
     '--engine', type=str, default='CYCLES',
     help='Blender internal engine for rendering. either CYCLES or EEVEE, ...')
+parser.add_argument(
+    '--gpu', type=int, default=0,
+    help='gpu.')
 
 argv = sys.argv[sys.argv.index("--") + 1:]
 args = parser.parse_args(argv)
@@ -43,6 +46,36 @@ else:
 context = bpy.context
 scene = bpy.context.scene
 render = bpy.context.scene.render
+
+def enable_cuda_devices():
+    prefs = bpy.context.preferences
+    cprefs = prefs.addons['cycles'].preferences
+    cprefs.get_devices()
+
+    # Attempt to set GPU device types if available
+    for compute_device_type in ('CUDA', 'OPENCL', 'NONE'):
+        try:
+            cprefs.compute_device_type = compute_device_type
+            print("Compute device selected: {0}".format(compute_device_type))
+            break
+        except TypeError:
+            pass
+
+    # Any CUDA/OPENCL devices?
+    acceleratedTypes = ['CUDA', 'OPENCL']
+    accelerated = any(device.type in acceleratedTypes for device in cprefs.devices)
+    print('Accelerated render = {0}'.format(accelerated))
+
+    # If we have CUDA/OPENCL devices, enable only them, otherwise enable
+    # all devices (assumed to be CPU)
+    print(cprefs.devices)
+    for idx, device in enumerate(cprefs.devices):
+        device.use = (not accelerated or device.type in acceleratedTypes) and idx == args.gpu
+        print('Device enabled ({type}) = {enabled}'.format(type=device.type, enabled=device.use))
+
+    return accelerated
+
+enable_cuda_devices()
 
 def bounds(obj, local=False):
     local_coords = obj.bound_box[:]
